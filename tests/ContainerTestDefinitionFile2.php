4e321865ed952c6c358f2a31bdfa2339465037b5
Eric Stern <eric@ericstern.com>
2019-02-22 16:15:01 -0800

    More comments in sample configs

--- tests/ContainerTestDefinitionFile2.php
+++ tests/ContainerTestDefinitionFile2.php
@@ -5,6 +5,10 @@ use function Firehed\Container\autowire;
 use Firehed\Container\Fixtures;
 
 return [
+    // Autowire a class with a required constructor param
     Fixtures\SessionHandler::class => autowire(),
+
+    // Autowire an interface to an implementation of a class with a required
+    // constructor param
     SessionHandlerInterface::class => Fixtures\SessionHandler::class,
 ];
