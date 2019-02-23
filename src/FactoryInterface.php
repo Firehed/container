d3a3b3241856ce55c486dab15cc00ed0c1c3afce
Eric Stern <eric@ericstern.com>
2019-02-22 16:45:58 -0800

    Make FactoryInterface useful

--- src/FactoryInterface.php
+++ src/FactoryInterface.php
@@ -3,10 +3,10 @@ declare(strict_types=1);
 
 namespace Firehed\Container;
 
+use Closure;
+
 interface FactoryInterface
 {
-    // getDefinition
-    // hasDefinition
-    // invoke
-
+    public function getDefinition(): Closure;
+    public function hasDefinition(): bool;
 }
