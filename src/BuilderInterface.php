49bb8fc7cde2886c5f71f72ce36e2897e24c5189
Eric Stern <eric@ericstern.com>
2026-01-05 13:11:28 -0800

    Add to interface

--- src/BuilderInterface.php
+++ src/BuilderInterface.php
@@ -5,6 +5,11 @@ namespace Firehed\Container;
 
 interface BuilderInterface
 {
+    /**
+     * @param mixed[] $defs
+     */
+    public function addDefinitions(array $defs): void;
+
     public function addFile(string $file): void;
 
     public function build(): TypedContainerInterface;
