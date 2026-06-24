4f52dd579245dc53ad6fc6fdc8d16ae3896b1e6c
Eric Stern <eric@ericstern.com>
2026-06-24 15:43:56 -0700

    Tests

--- src/BuilderInterface.php
+++ src/BuilderInterface.php
@@ -6,6 +6,9 @@ namespace Firehed\Container;
 
 interface BuilderInterface
 {
+    /**
+     * Add all `.php` files in the directory to the builder. Not recursive.
+     */
     public function addDirectory(string $directory): void;
 
     public function addFile(string $file): void;
